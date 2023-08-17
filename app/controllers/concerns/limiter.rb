463e3ac674dc5c45c33c4f98716799d7cf9a46fa
shiedakayn1975 <allmid66@gmail.com>
2023-08-18 00:16:28 +0700

    add rule limit ip address

--- app/controllers/concerns/limiter.rb
+++ app/controllers/concerns/limiter.rb
@@ -1,7 +1,8 @@
 module Limiter
   extend ActiveSupport::Concern
-  
+
   def check_limitation
-    TokenBucketManager.new
+    token_bucket = TokenBucketManager.new(request.remote_ip)
+    token_bucket.check_rate_limitation_with_ip_address
   end
 end
--- app/services/token_bucket_manager.rb
+++ app/services/token_bucket_manager.rb
@@ -1,5 +1,13 @@
 class TokenBucketManager
-  def initialize
+  RULE_FILE_PATH = "config/rate_limit_rule.yaml"
 
+  def initialize ip
+    @ip = ip
+  end
+
+  def check_rate_limitation_with_ip_address
+    @rule = Rails.cache.fetch(["limiter", "ip_address"], expires_in: 12.hours) do
+      YAML.load_file(RULE_FILE_PATH)
+    end
   end
 end
